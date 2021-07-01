3bac8743f4616301b2bb91056aeb96df5b42f4bc
Daniel Farias <danielmfr6@gmail.com>
2021-07-01 14:44:17 -0300

    Ajustar repository para comunicar com firebase

--- app/src/main/java/com/vbuecker/dev_venture_whatsapp/data/repository/UserRepository.kt
+++ app/src/main/java/com/vbuecker/dev_venture_whatsapp/data/repository/UserRepository.kt
@@ -1,11 +1,17 @@
 package com.vbuecker.dev_venture_whatsapp.data.repository
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.vbuecker.dev_venture_whatsapp.data.model.User
 
+private const val TAG = "UserRepository"
+
 object UserRepository {
-    private val db by lazy { Firebase.firestore}
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val db by lazy { Firebase.firestore }
 
     fun addUser(user: User, onSuccess: () -> Unit, onFail: (error: String) -> Unit) {
         db.collection("users")
@@ -14,8 +20,30 @@ object UserRepository {
             .addOnSuccessListener {
                 onSuccess()
             }
-            .addOnFailureListener{ e ->
+            .addOnFailureListener { e ->
                 onFail(e.localizedMessage)
             }
+    }
+
+    fun checkIfUserIsAuthenticated(): MutableLiveData<Boolean> {
+        val isUserAuthenticated = MutableLiveData<Boolean>()
+        val firebaseUser = firebaseAuth.currentUser
+        isUserAuthenticated.value = firebaseUser != null
+        return isUserAuthenticated
+    }
+
+    fun saveUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        val user = User(
+            firebaseUser?.displayName ?: "No name",
+            firebaseUser?.email ?: "No email",
+            firebaseUser!!.uid
+        )
+        db.collection("users")
+            .document(user.email)
+            .set(user)
+            /*TODO: Adicionar mensagem de sucesso e erro */
+            .addOnSuccessListener { Log.d(TAG, "User salvo") }
+            .addOnFailureListener { e -> Log.d(TAG, "Falha ao salvar ${e.localizedMessage} salvo") }
     }
 }
