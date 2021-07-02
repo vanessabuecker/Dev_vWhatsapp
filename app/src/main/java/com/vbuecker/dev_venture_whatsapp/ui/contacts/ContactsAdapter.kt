4fb0f6396836d7181830499acb4594e8e626475d
Daniel Farias <danielmfr6@gmail.com>
2021-07-01 23:27:23 -0300

    Criar adapter para popular recyclerview
    - Criar entidade de contato
    - Criar layout de viewholder

---
+++ app/src/main/java/com/vbuecker/dev_venture_whatsapp/ui/contacts/ContactsAdapter.kt
@@ -0,0 +1,43 @@
+package com.vbuecker.dev_venture_whatsapp.ui.contacts
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.vbuecker.dev_venture_whatsapp.R
+import com.vbuecker.dev_venture_whatsapp.data.model.Contact
+
+class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.MyViewHolder>() {
+
+    var contactsList = arrayListOf<Contact>()
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val contactName: TextView = itemView.findViewById(R.id.contact_name)
+        private val contactDetail: TextView = itemView.findViewById(R.id.contact_detail)
+
+        fun bind(contact: Contact) {
+            contactName.text = contact.name
+            contactDetail.text = contact.email
+//            contactImage = TODO(Setar imagem)
+        }
+    }
+
+    fun setDataset(contacts: ArrayList<Contact>) {
+        contactsList = contacts
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_contact, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(contactsList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return contactsList.size
+    }
+}
