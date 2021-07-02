358a882f5a76909f93d65f3bfdbc03117c2efa10
Daniel Farias <danielmfr6@gmail.com>
2021-07-01 23:27:49 -0300

    Configurar repository para recuperar dados do firestore

---
+++ app/src/main/java/com/vbuecker/dev_venture_whatsapp/ui/contacts/ContactsViewModel.kt
@@ -0,0 +1,19 @@
+package com.vbuecker.dev_venture_whatsapp.ui.contacts
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.vbuecker.dev_venture_whatsapp.data.model.Contact
+import com.vbuecker.dev_venture_whatsapp.data.repository.UserRepository
+
+class ContactsViewModel(val userRepository: UserRepository) : ViewModel() {
+
+    private val _contactsList = MutableLiveData<ArrayList<Contact>>().apply {
+        userRepository.getMyContact {
+            value = it
+        }
+    }
+
+    val contactsList: LiveData<ArrayList<Contact>>
+        get() = _contactsList
+}
