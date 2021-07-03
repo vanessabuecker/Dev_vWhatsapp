31230db5883b25a5228ccda4f397f74c05fd04e4
Daniel Farias <danielmfr6@gmail.com>
2021-07-03 14:08:31 -0300

    Ajustar chamadas via viewmodel

--- app/src/main/java/com/vbuecker/dev_venture_whatsapp/ui/chat/ChatsFragment.kt
+++ app/src/main/java/com/vbuecker/dev_venture_whatsapp/ui/chat/ChatsFragment.kt
@@ -8,14 +8,11 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import com.vbuecker.dev_venture_whatsapp.data.repository.ChatRepository
 import com.vbuecker.dev_venture_whatsapp.databinding.FragmentChatsBinding
-import com.vbuecker.dev_venture_whatsapp.ui.login.LoginViewModel
 
 class ChatsFragment : Fragment() {
     private var _binding: FragmentChatsBinding? = null
-    private val binding
-        get() = _binding!!
+    private val binding get() = _binding!!
     private val viewModel: ChatsViewModel by viewModel()
     private lateinit var mAdapter: ChatsAdapter
     private var chatId: String? = null
@@ -25,9 +22,7 @@ class ChatsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentChatsBinding.inflate(inflater, container, false)
-        chatId = arguments?.getString("chatId")
-        if (chatId.isNullOrEmpty())
-            findNavController().popBackStack()
+        checkIfChatIdExists()
         return binding.root
     }
 
@@ -36,6 +31,12 @@ class ChatsFragment : Fragment() {
         initRecyclerView()
         configureSendMessage()
         subscribeObservers()
+    }
+
+    private fun checkIfChatIdExists() {
+        chatId = arguments?.getString("chatId")
+        if (chatId.isNullOrEmpty())
+            findNavController().popBackStack()
     }
 
     private fun initRecyclerView() {
@@ -49,7 +50,7 @@ class ChatsFragment : Fragment() {
         val myEmail = viewModel.myEmail
         binding.btnSend.setOnClickListener {
             val msg = binding.tvMessage.text.toString()
-            ChatRepository.addMessageToChat(chatId!!, myEmail, msg)
+            viewModel.addMessageToChat(chatId!!, myEmail, msg)
         }
     }
 
